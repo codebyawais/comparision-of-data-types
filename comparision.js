a85cbbd1933488a56744bbdd3b3954dc5416a36a
WebDevByAwaisRafiq <webdevbyawais@gmail.com>
2024-06-05 07:43:35 +0500

    Add files via upload

---
+++ comparision.js
@@ -0,0 +1,27 @@
+// console.log(2<3);
+// console.log(3<2);
+
+
+// console.log("2" < 3);
+// console.log(2<"3");
+// console.log("2"<"3");
+// console.log(2 < 3);
+// console.log(2 > 3);
+
+
+// console.log(null < 0);
+// console.log(null <= 0);
+// console.log(null > 0);
+
+
+console.log(undefined < 0);
+console.log(undefined <= 0);
+console.log(undefined > 0);
+
+
+
+console.log(undefined < null);
+console.log(undefined <= null);
+console.log(undefined > null);
+
+
